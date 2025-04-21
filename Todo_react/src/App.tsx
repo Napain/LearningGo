32f0089c1db879b5479f9b345317daee09370d3f
Napain <storrealba24@gmail.com>
2025-04-21 17:54:40 +0200

    Before deployment

---
+++ Todo_react/src/App.tsx
@@ -0,0 +1,21 @@
+import { Container, Stack } from "@chakra-ui/react";
+import Header from "./components/header";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/todoList";
+
+export const BASE_URL =
+  import.meta.env.Mode === "development" ? "http://127.0.0.1:5000/api" : "/api";
+
+function App() {
+  return (
+    <Stack>
+      <Container>
+        <Header />
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack>
+  );
+}
+
+export default App;
---
+++ Todo_react/src/components/header.tsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <h1> Naps </h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
