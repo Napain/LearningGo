115640da8aea749105875b85e4856eb2a6f023fc
Napain <storrealba24@gmail.com>
2025-04-21 18:23:54 +0200

    Fixing deployment bug

--- Todo_react/src/App.tsx
+++ Todo_react/src/App.tsx
@@ -4,7 +4,7 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/todoList";
 
 export const BASE_URL =
-  import.meta.env.Mode === "development" ? "http://127.0.0.1:5000/api" : "/api";
+  import.meta.env.Mode === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
